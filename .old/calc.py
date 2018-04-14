0d56e70de238f16ca50ca4254ec9550ef3d0a873
Christian Kuhl <christian.kuhl84@gmail.com>
2018-04-14 13:05:09 +0200

    Cleanup.

--- .old/calc.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from klausur import Klausur
-from iodevices import File, HTML, Terminal
-
-if __name__ == '__main__':
-    with Klausur(input=HTML, output=HTML) as K:
-        K.input()
-        # K.calculate()
-        # K.output()
--- klausur/settings.py
+++ klausur/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from klausur.private import SECRETKEY, DEBUG_FLAG
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=-o$_)&24vgrsx-mclf6jkna-e_gt%unb+2v^1lkug+20qc-yo'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG_FLAG
 
 ALLOWED_HOSTS = []
 
