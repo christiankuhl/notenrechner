92416959194a7e84884f7f5586a1b317b53c8ed9
Christian Kuhl <christian.kuhl84@gmail.com>
2019-12-21 22:54:42 +0100

    Various.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn notenrechner.wsgi
--- klausur/settings.py
+++ klausur/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from klausur.private import SECRETKEY, DEBUG_FLAG, ALLOWEDHOSTS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRETKEY
+SECRET_KEY = "ChangeMe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = DEBUG_FLAG
-
-ALLOWED_HOSTS = ALLOWEDHOSTS
+DEBUG = True
 
 # Application definition
 
